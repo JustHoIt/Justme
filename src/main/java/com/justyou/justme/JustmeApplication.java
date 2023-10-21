6e09e0520ff9162673d80056d8bd8522e4efac84
Park <zalop3128@kakao.com>
2023-10-21 19:04:08 +0900

    Initial setting

---
+++ src/main/java/com/justyou/justme/JustmeApplication.java
@@ -0,0 +1,13 @@
+package com.justyou.justme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustmeApplication.class, args);
+	}
+
+}
