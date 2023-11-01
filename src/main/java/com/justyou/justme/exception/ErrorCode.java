b42c8e0ae0becd5218c00caf30b1d7b4f8db25df
Park <zalop3128@kakao.com>
2023-11-02 07:29:20 +0900

    FEAT : ADD - LOGIN 1. 로그인 기능 구현

--- src/main/java/com/justyou/justme/exception/ErrorCode.java
+++ src/main/java/com/justyou/justme/exception/ErrorCode.java
@@ -10,7 +10,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     VALIDATION_FAILED(HttpStatus.BAD_REQUEST, ""),
     ALREADY_REGISTERED_EMAIL(HttpStatus.BAD_REQUEST, "이미 가입된 이메일 주소 입니다."),
-    ALREADY_REGISTERED_PHONE(HttpStatus.BAD_REQUEST, "이미 가입된 휴대폰 번호 입니다.");
+    ALREADY_REGISTERED_PHONE(HttpStatus.BAD_REQUEST, "이미 가입된 휴대폰 번호 입니다."),
+    NOT_MATCH_USER(HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호가 일치하지 않습니다."),
+    NOT_EMAIL_AUTHENTICATE_USER(HttpStatus.BAD_REQUEST, "이메일 인증이 완료되지 않은 유저입니다."),
+    WITHDRAW_USER(HttpStatus.BAD_REQUEST, "회원탈퇴한 유저입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/justyou/justme/service/MemberService.java
+++ src/main/java/com/justyou/justme/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.justyou.justme.service;
 
 
+import com.justyou.justme.dto.LogInDto;
 import com.justyou.justme.dto.RequestMemberSignUpDto;
 import com.justyou.justme.dto.ResponseDto;
 import com.justyou.justme.exception.CustomException;
@@ -9,16 +10,23 @@ import com.justyou.justme.model.entity.Member;
 import com.justyou.justme.model.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public ResponseDto signUp(RequestMemberSignUpDto dto) {
@@ -55,4 +63,32 @@ public class MemberService {
     public boolean checkEmail(String email) {
         return this.memberRepository.existsByEmail(email);
     }
+
+    public Member authenticate(LogInDto form) {
+        /*1. 유저 아이디 검색 후 일치하는 아이디 확인한뒤 없으면 NOT_MATCH_USER EXCEPTION 처리
+         * 2. 입력한 비밀번호와 회원 비밀번호를 비교하여 일치 하지않으면  NOT_MATCH_USER EXCEPTION 처리(비밀번호가 틀렸다고 하면 다른사람이 시도할 수 있음)
+         * 3. 유저의 상태를 보고 인증전, 회원탈퇴 유저는 EXCEPTION 처리 */
+        Optional<Member> optionalMember = memberRepository.findByEmail(form.getEmail());
+        if (optionalMember.isEmpty()) {
+            throw new CustomException(ErrorCode.NOT_MATCH_USER);
+        }
+        Member member = optionalMember.get();
+        if (!this.passwordEncoder.matches(form.getPassword(), member.getPassword())) {
+            throw new CustomException(ErrorCode.NOT_MATCH_USER);
+        }
+        if (member.getUserStatus().equals("REQ")) {
+            throw new CustomException(ErrorCode.NOT_EMAIL_AUTHENTICATE_USER);
+        }
+        if (member.getUserStatus().equals("WITHDRAW")) {
+            throw new CustomException(ErrorCode.WITHDRAW_USER);
+        }
+
+        return member;
+    }
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) this.memberRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다." + username));
+    }
+
 }
